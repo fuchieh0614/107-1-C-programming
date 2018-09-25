d7c70b3691d3d9733e66eefcd6e3a4eba22990f4
fuchieh0614 <43245417+fuchieh0614@users.noreply.github.com>
2018-09-25 17:46:21 +0800

    create BMI.cpp

---
+++ w03/BMI.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	float height,weight;
+	double BMI;
+	printf("Enter height: ");
+	scanf("%f",&height);
+	printf("Enter weight: ");
+	scanf("%f",&weight);
+	while(weight>=0&&weight>=0){
+		BMI=weight/((height/100)*(height/100));
+		printf("BMI=%.1f\n",BMI);
+		if(BMI<18.5)
+	   		printf("體重過輕\n");
+	   	else if(18.5<=BMI&&BMI<24)
+	   	    printf("體重正常\n");
+	   	else if(24<=BMI&&BMI<27)
+	   	    printf("體重過重\n");
+	    else if(27<=BMI&&BMI<30)
+		    printf("輕度肥胖\n");
+		else if(30<=BMI&&BMI<35)
+		    printf("中度肥胖\n");
+		else if(BMI>=35)
+		    printf("重度肥胖\n");
+		printf("Enter height: ");
+	    scanf("%f",&height);
+		printf("Enter weight: ");
+		scanf("%f",&weight); 				
+	}		
+	return 0;
+}
