7f0f863c4d334a9e97dda685c25cf2dfc3018d95
fuchieh0614 <43245417+fuchieh0614@users.noreply.github.com>
2018-10-19 15:42:19 +0800

    Create convert seconds using while loop.cpp

---
+++ w03/convert seconds using while loop.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int seconds;
+	int hh,mm,ss;
+	printf("Enter seconds: ");
+	scanf("%d",&seconds);
+	while(seconds>=0){
+		hh=seconds/3600;
+		mm=seconds%3600/60;
+		ss=seconds%60;
+		printf("%d seconds = %02d:%02d:%02d\n",seconds,hh,mm,ss);
+
+	        printf("Enter seconds: ");
+		scanf("%d",&seconds);		
+	}
+	return 0;
+}
