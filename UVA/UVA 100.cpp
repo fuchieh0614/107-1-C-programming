8d18c5325c9a19717f1f7e8ba10531ed7e794693
fuchieh0614 <43245417+fuchieh0614@users.noreply.github.com>
2018-11-25 19:49:17 +0800

    Update UVA 100.cpp

--- UVA/UVA 100.cpp
+++ UVA/UVA 100.cpp
@@ -1,1 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,n;
+    while(scanf("%d %d",&a,&b)!=EOF){
+        int max=0;
+        int s,e;
+        if(a<b){
+            s=a;
+            e=b;
+        }else{
+            s=b;
+            e=a;
+        }
+        for(int i=s; i<=e; i++)
+        {
+            int length=1;
+            int c=i;
+            while(c>1)
+            {
+                if (c%2==0)
+                {
+                    n=c/2;
+                    length++;
+                }
+                else
+                {
+                    n=c*3+1;
+                    length++;
+                }
+                if(n==1)
+                    break;
+                c=n;
+            }
+            if(length > max)
+            {
+                max = length;
+            }
+        }
+        printf("%d %d %d\n",a,b,max);
+    }
+    return 0;
+}
 
