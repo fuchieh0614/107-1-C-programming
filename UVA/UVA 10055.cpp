921ef9b2140ace8626238fc9d2e251cf2bb0bafe
fuchieh0614 <43245417+fuchieh0614@users.noreply.github.com>
2018-12-09 22:50:00 +0800

    Update UVA 10055.cpp

--- UVA/UVA 10055.cpp
+++ UVA/UVA 10055.cpp
@@ -3,9 +3,10 @@
 int main()
 {
     long long a,b;
-    while(scanf("%d %d",&a,&b)!=EOF)
+    while(scanf("%lld %lld",&a,&b)!=EOF)
     {
-        if(a<=b)printf("%d\n",b-a);
+        if(a<=b)printf("%lld\n",b-a);
+        else printf("%lld\n",a-b);
     }
     return 0;
 }
